4726700f856d7bc7e063cf10c212f62425bbb4bd
Joshua Brunscheen <114787256+josh-brunscheen@users.noreply.github.com>
2024-10-05 00:51:05 -0400

    Got rid of search icon (not used)

--- src/Header.jsx
+++ src/Header.jsx
@@ -40,9 +40,9 @@ function Header() {
           <a className="ml-2 mr-2">Projects</a>
         </div> */}
 
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="cursor-pointer bi bi-search mt-1 ml-2 mr-2 transition duration-500 ease-in-out hover:text-blue-500" viewBox="0 0 16 16">
+        {/* <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="cursor-pointer bi bi-search mt-1 ml-2 mr-2 transition duration-500 ease-in-out hover:text-blue-500" viewBox="0 0 16 16">
           <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-        </svg>
+        </svg> */}
       
         <svg onClick={toggleTheme} xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="cursor-pointer bi bi-circle-half mt-1 ml-2 mr-2 transition duration-500 ease-in-out hover:text-blue-500" viewBox="0 0 16 16">
           <path d="M8 15A7 7 0 1 0 8 1zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16"/>
